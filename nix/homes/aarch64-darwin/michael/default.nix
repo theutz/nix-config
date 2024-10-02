44670e25b0f91cf855c2819017c5d59fc1588bd9
Michael Utz <michael@theutz.com>
2024-10-03 01:17:24 +0300

    chore: add neovim

--- nix/homes/aarch64-darwin/michael/default.nix
+++ nix/homes/aarch64-darwin/michael/default.nix
@@ -1,3 +1,15 @@
-_: {
+{ pkgs, inputs, target, ... }: let
+  unstable = inputs.unstable.legacyPackages."${target}";
+in {
   home.stateVersion = "24.05";
+  
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "neovide";
+  };
+
+  home.packages = with pkgs; [
+    neovide
+    unstable.neovim
+  ];
 }
