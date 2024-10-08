4e7ff4906bba208a9a7595da45f318776de6b45b
Michael Utz <michael@theutz.com>
2024-10-08 19:35:34 +0300

    fix(zsh): turn off extended glob / interactive comments
    Signed-off-by: Michael Utz <michael@theutz.com>

--- nix/modules/home/zsh/default.nix
+++ nix/modules/home/zsh/default.nix
@@ -7,10 +7,10 @@
   inherit (lib) mkIf mkEnableOption mkMerge;
 
   mod = "zsh";
-  cfg = config."${namespace}"."${mod}";
+  cfg = config.${namespace}.${mod};
 in {
-  options."${namespace}"."${mod}" = {
-    enable = mkEnableOption "zsh settings";
+  options.${namespace}.${mod} = {
+    enable = mkEnableOption "the zshell";
   };
 
   config = mkIf cfg.enable {
@@ -22,6 +22,10 @@ in {
           lg = "lazygit";
         })
       ];
+
+      initExtra = ''
+        setopt NO_EXTENDED_GLOB NO_INTERACTIVE_COMMENTS
+      '';
     };
   };
 }
