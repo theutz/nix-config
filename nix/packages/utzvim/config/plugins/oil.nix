1a3abd0991759795e16a0fc0939aa76938482418
Michael Utz <michael@theutz.com>
2024-10-23 13:17:20 +0300

    feat(oil): localleader keybinds
    Signed-off-by: Michael Utz <michael@theutz.com>

--- nix/packages/utzvim/config/plugins/oil.nix
+++ nix/packages/utzvim/config/plugins/oil.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  helpers,
   ...
 }: let
   inherit (lib) mkIf;
@@ -10,8 +11,46 @@ in {
     enable = true;
     settings = {
       keymaps = {
-        "q" = "actions.close";
+        "<localleader>?" = "actions.show_help";
+        "<localleader>o" = "actions.select";
+        "<localleader>v" =
+          helpers.listToUnkeyedAttrs ["actions.select"]
+          // {
+            opts = {vertical = true;};
+            desc = "Open entry in a vertical split";
+          };
+        "<localleader>s" =
+          helpers.listToUnkeyedAttrs ["actions.select"]
+          // {
+            opts = {horizontal = true;};
+            desc = "Open entry in a horizontal split";
+          };
+        "<localleader>t" =
+          helpers.listToUnkeyedAttrs ["actions.select"]
+          // {
+            opts = {tab = true;};
+            desc = "Open entry in a new tab";
+          };
+        "<localleader>p" = "actions.preview";
+        "<localleader>c" = "actions.close";
+        "<localleader>r" = "actions.refresh";
+        "<cr>" = "actions.select";
+        "-" = "actions.parent";
+        "`" = "actions.cd";
+        "_" = "actions.open_cwd";
+        "~" =
+          helpers.listToUnkeyedAttrs ["actions.cd"]
+          // {
+            opts = {scope = "tab";};
+            desc = ":tcd to the current oil directory";
+            mode = "n";
+          };
+        "<localleader>S" = "actions.change_sort";
+        "<localleader>O" = "actions.open_external";
+        "<localleader>." = "actions.toggle_hidden";
+        "<localleader>\\" = "actions.toggle_trash";
       };
+      use_default_keymaps = false;
       view_options = {
         show_hidden = true;
       };
