9ee01d1b0661ac5fe78365bd01a5320bbd6d2689
Michael Utz <michael@theutz.com>
2024-10-23 13:17:38 +0300

    fix(toggleterm): close easier
    Signed-off-by: Michael Utz <michael@theutz.com>

--- nix/packages/utzvim/config/plugins/toggleterm.nix
+++ nix/packages/utzvim/config/plugins/toggleterm.nix
@@ -1,4 +1,10 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.plugins.toggleterm;
+in {
   plugins.toggleterm = {
     enable = true;
     settings = {
@@ -7,4 +13,25 @@
       terminal_mappings = true;
     };
   };
+
+  autoCmd = lib.mkIf cfg.enable [
+    {
+      event = ["TermEnter"];
+      # If you don't add this autocmd, then ToggleTerm can prevent the normal
+      # exit behavior for vim when running :wq, :wqa, :wq!, and :wqa!
+      callback.__raw = ''
+        function()
+          for _, buffers in ipairs(vim.fn.getbufinfo()) do
+            local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, "filetype")
+            if filetype == "toggleterm" then
+              vim.api.nvim_create_autocmd({ "BufWriteCmd", "FileWriteCmd", "FileAppendCmd" }, {
+                buffer = buffers.bufnr,
+                command = "q!",
+              })
+            end
+          end
+        end
+      '';
+    }
+  ];
 }
