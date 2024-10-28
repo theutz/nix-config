e5d6bc179d45f54e24256da1ed95a8748062985b
Michael Utz <michael@theutz.com>
2024-10-28 20:24:18 +0300

    Revert "feat: add darwin-config to nix path"
    This reverts commit 07d797fc15f5987b9c9434de684c2f663a6d74e5.

--- nix/systems/aarch64-darwin/kocaeli/nix.nix
+++ nix/systems/aarch64-darwin/kocaeli/nix.nix
@@ -1,10 +1,6 @@
 {
   nix = {
-    nixPath = {
-      nixpkgs = "flake:nixpkgs";
-      darwin-config = "/etc/darwin-config";
-      unstable = "github:nixpkgs/nixpkgs-unstable";
-    };
+    nixPath = {nixpkgs = "flake:nixpkgs";};
 
     settings = {
       experimental-features = ["nix-command" "flakes"];
