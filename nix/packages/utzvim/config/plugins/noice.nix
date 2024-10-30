dacd739ced24831f385c59e8fa81fb5babe29050
Michael Utz <michael@theutz.com>
2024-10-31 02:00:20 +0300

    Generation 1093

--- nix/packages/utzvim/config/plugins/noice.nix
+++ nix/packages/utzvim/config/plugins/noice.nix
@@ -8,13 +8,10 @@ in {
   plugins.noice = {
     enable = true;
     presets = {
-      # bottom_search = true;
-      # command_palette = true;
+      bottom_search = true;
+      command_palette = true;
       long_message_to_split = true;
       inc_rename = true;
-    };
-    cmdline = {
-      view = "cmdline";
     };
     routes = [
       {
