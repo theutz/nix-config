515a77ce5e04ccaef51cee0f0f2f7f74aa750c70
Michael Utz <michael@theutz.com>
2024-10-31 01:59:25 +0300

    Generation 1092

--- nix/packages/utzvim/config/plugins/noice.nix
+++ nix/packages/utzvim/config/plugins/noice.nix
@@ -24,25 +24,6 @@ in {
     ];
   };
 
-  # keymaps = lib.mkMerge [
-  #   (lib.mkIf config.plugins.noice.enable [
-  #     {
-  #       mode = "n";
-  #       key = "<leader>ln";
-  #       action = "<cmd>NoiceAll<cr>";
-  #       options.desc = "Notifications";
-  #     }
-  #   ])
-  #   (lib.mkIf config.plugins.fzf-lua.enable [
-  #     {
-  #       mode = "n";
-  #       key = "<leader>sn";
-  #       action = "<cmd>Noice fzf<cr>";
-  #       options.desc = "Notifications";
-  #     }
-  #   ])
-  # ];
-
   keymaps = lib.concatLists [
     (lib.optionals cfg.enable [
       {
