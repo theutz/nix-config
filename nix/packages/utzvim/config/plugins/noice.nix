08572d0137b8f695265fc06841564e6566170bda
Michael Utz <michael@theutz.com>
2024-10-31 01:48:44 +0300

    Generation 1088

--- nix/packages/utzvim/config/plugins/noice.nix
+++ nix/packages/utzvim/config/plugins/noice.nix
@@ -5,13 +5,19 @@
 }: {
   plugins.noice = {
     enable = true;
-    cmdline = {
-      view = "cmdline";
-      format = {
-        search_down = {view = "cmdline";};
-        search_up = {view = "cmdline";};
-      };
+    presets = {
+      bottom_search = true;
+      command_palette = true;
+      long_message_to_split = true;
+      inc_rename = true;
     };
+    # cmdline = {
+    # view = "cmdline";
+    # format = {
+    #   search_down = {view = "cmdline";};
+    #   search_up = {view = "cmdline";};
+    # };
+    # };
     routes = [
       {
         view = "notify";
