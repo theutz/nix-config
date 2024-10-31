a58d05fb79eee86934d53e70838fb714e9b61bbe
Michael Utz <michael@theutz.com>
2024-10-31 11:23:26 +0300

    Generation 1095

--- nix/packages/utzvim/config/plugins/lualine.nix
+++ nix/packages/utzvim/config/plugins/lualine.nix
@@ -3,16 +3,16 @@
     enable = true;
     settings = {
       sections = {
-        lualine_c = [
-          # {
-          #   __raw = ''
-          #     {
-          #       require("noice").api.statusline.mode.get,
-          #       cond = require("noice").api.statusline.mode.has,
-          #       color = { fg = "#ff9e64" },
-          #     }
-          #   '';
-          # }
+        lualine_x = [
+          {
+            __raw = ''
+              {
+                require("noice").api.statusline.mode.get,
+                cond = require("noice").api.statusline.mode.has,
+                color = { fg = "#ff9e64" },
+              }
+            '';
+          }
         ];
       };
     };
