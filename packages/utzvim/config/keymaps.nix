d89c101d311f2a835ee7f7272c14d3b2cc701b6b
Michael Utz <michael@theutz.com>
2024-11-05 11:52:13 +0300

    Generation 1202

--- packages/utzvim/config/keymaps.nix
+++ packages/utzvim/config/keymaps.nix
@@ -1,141 +1,46 @@
 {lib, ...}: let
-  mkWinKey = mode: key: desc:
+  mkWinKey' = mode: key: desc:
     assert (lib.isList key || lib.isString key); let
       pressed =
         if lib.isList key
-        then lib.elemAt 0
+        then lib.elemAt key 0
         else key;
       sent =
         if lib.isList key
-        then lib.elemAt 1
+        then lib.elemAt key 1
         else key;
     in {
       inherit mode;
       key = "<leader>w${pressed}";
-      action = "<cmd>wincmd ${sent}";
+      action = "<cmd>wincmd ${sent}<cr>";
       options = {inherit desc;};
     };
-  mkWinKeyN = mkWinKey "n";
+
+  mkWinKey = mkWinKey' "n";
 
   winKeys = [
-    (mkWinKeyN "+" "Increase height")
-    # {
-    #   mode = "n";
-    #   key = "<leader>w+";
-    #   action = "<cmd>wincmd +<cr>";
-    #   options.desc = "Increase height";
-    # }
-    {
-      mode = "n";
-      key = "<leader>w-";
-      action = "<cmd>wincmd -<cr>";
-      options.desc = "Decrease height";
-    }
-    {
-      mode = "n";
-      key = "<leader>w<";
-      action = "<cmd>wincmd <<cr>";
-      options.desc = "Decrease width";
-    }
-    {
-      mode = "n";
-      key = "<leader>w=";
-      action = "<cmd>wincmd =<cr>";
-      options.desc = "Equal width and height";
-    }
-    {
-      mode = "n";
-      key = "<leader>w>";
-      action = "<cmd>wincmd ><cr>";
-      options.desc = "Increase width";
-    }
-    {
-      mode = "n";
-      key = "<leader>w_";
-      action = "<cmd>wincmd _<cr>";
-      options.desc = "Max out the height";
-    }
-    {
-      mode = "n";
-      key = "<leader>wh";
-      action = "<cmd>wincmd h<cr>";
-      options.desc = "Focus left window";
-    }
-    {
-      mode = "n";
-      key = "<leader>wj";
-      action = "<cmd>wincmd j<cr>";
-      options.desc = "Focus window below";
-    }
-    {
-      mode = "n";
-      key = "<leader>wk";
-      action = "<cmd>wincmd k<cr>";
-      options.desc = "Focus window above";
-    }
-    {
-      mode = "n";
-      key = "<leader>wl";
-      action = "<cmd>wincmd l<cr>";
-      options.desc = "Focus right window";
-    }
-    {
-      mode = "n";
-      key = "<leader>wo";
-      action = "<cmd>wincmd o<cr>";
-      options.desc = "Close all others";
-    }
-    {
-      mode = "n";
-      key = "<leader>wq";
-      action = "<cmd>wincmd q<cr>";
-      options.desc = "Close window";
-    }
-    {
-      mode = "n";
-      key = "<leader>wd";
-      action = "<cmd>wincmd q<cr>";
-      options.desc = "Close window";
-    }
-    {
-      mode = "n";
-      key = "<leader>ws";
-      action = "<cmd>wincmd s<cr>";
-      options.desc = "Split window horizontally";
-    }
-    {
-      mode = "n";
-      key = "<leader>wT";
-      action = "<cmd>wincmd T<cr>";
-      options.desc = "Break into new tab";
-    }
-    {
-      mode = "n";
-      key = "<leader>wv";
-      action = "<cmd>wincmd v<cr>";
-      options.desc = "Split window vertically";
-    }
-    {
-      mode = "n";
-      key = "<leader>ww";
-      action = "<cmd>wincmd w<cr>";
-      options.desc = "Switch windows";
-    }
-    {
-      mode = "n";
-      key = "<leader>wx";
-      action = "<cmd>wincmd x<cr>";
-      options.desc = "Swap current with next";
-    }
-    {
-      mode = "n";
-      key = "<leader>w|";
-      action = "<cmd>wincmd |<cr>";
-      options.desc = "Max out the width";
-    }
+    (mkWinKey "+" "Increase height")
+    (mkWinKey "-" "Decrease height")
+    (mkWinKey ">" "Increase width")
+    (mkWinKey "<" "Decrease width")
+    (mkWinKey "=" "Equal width/height")
+    (mkWinKey "_" "Max height")
+    (mkWinKey "|" "Max width")
+    (mkWinKey "h" "Focus left")
+    (mkWinKey "j" "Focus below")
+    (mkWinKey "k" "Focus above")
+    (mkWinKey "l" "Focus right")
+    (mkWinKey "o" "Close other windows")
+    (mkWinKey "q" "Close this window")
+    (mkWinKey ["d" "q"] "Close this window")
+    (mkWinKey "s" "Split horizontal")
+    (mkWinKey "v" "Split vertical")
+    (mkWinKey "T" "Break into tab")
+    (mkWinKey "w" "Focus previous")
+    (mkWinKey "x" "Swap with next")
   ];
 
-  mkPair = mode: key: name: prev: next: [
+  mkPair' = mode: key: name: prev: next: [
     {
       inherit mode;
       key = "[${key}";
@@ -150,12 +55,12 @@
     }
   ];
 
-  mkPairN = mkPair "n";
+  mkPair = mkPair' "n";
   pairs = lib.concatLists [
-    (mkPairN "b" "buffer" "<cmd>bprev<cr>" "<cmd>bnext<cr>")
-    (mkPairN "<tab>" "tab" "<cmd>tabprev<cr>" "<cmd>tabnext<cr>")
-    (mkPairN "q" "quickfix" "<cmd>cprev<cr>" "<cmd>cnext<cr>")
-    (mkPairN "l" "location" "<cmd>lprev<cr>" "<cmd>lnext<cr>")
+    (mkPair "b" "buffer" "<cmd>bprev<cr>" "<cmd>bnext<cr>")
+    (mkPair "<tab>" "tab" "<cmd>tabprev<cr>" "<cmd>tabnext<cr>")
+    (mkPair "q" "quickfix" "<cmd>cprev<cr>" "<cmd>cnext<cr>")
+    (mkPair "l" "location" "<cmd>lprev<cr>" "<cmd>lnext<cr>")
   ];
 
   bufferKeys = lib.concatLists [
