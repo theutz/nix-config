757031d0e66990df6e0e442a8142006a6585ec6e
Michael Utz <michael@theutz.com>
2024-11-07 15:53:52 +0300

    Generation 1234

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -13,10 +13,12 @@
       };
 
       settings = {
+        auto-optimise-store = true;
+
         experimental-features = [
           "nix-command"
           "flakes"
-          "pipe-operator"
+          "pipe-operators"
         ];
 
         trusted-users = [
