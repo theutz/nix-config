d34687efd246c9f2e490158412a135927b72c0df
Michael Utz <michael@theutz.com>
2024-11-08 21:54:05 +0300

    Generation 84

--- modules/home/aerospace/default.nix
+++ modules/home/aerospace/default.nix
@@ -20,12 +20,14 @@
     inactive = "0x88a9b1d6";
   };
 
-  mkArgs = lib.cli.toGNUCommandLineShell {};
+  mkFlags = lib.cli.toGNUCommandLineShell {};
+  mkArgs = args: lib.concatStringsSep " " (lib.mapAttrsToList (name: value: "${name}=${builtins.toString value}") args);
+
   defaultBorders = {
     active_color = colors.active;
     inactive_color = colors.inactive;
-    blur_radius = 12;
-    width = 8.0;
+    blur_radius = "12";
+    width = "8.0";
     hidipi = true;
     style = "round";
   };
@@ -37,15 +39,12 @@
     after-startup-command =
       lib.forEach [
         [borders (mkArgs defaultBorders)]
-        [sketchybar (mkArgs {reload = true;})]
+        [sketchybar (mkFlags {reload = true;})]
       ] (x:
         lib.pipe x [
           (lib.concat ["exec-and-forget"])
           (lib.concatStringsSep " ")
         ]);
-    # after-startup-command =
-    #   lib.forEach [
-    #   ] (lib.pipe) (x: "exec-and-forget ${x}");
   };
 in {
   options.internal.${mod} = {
@@ -54,7 +53,7 @@ in {
 
   config = lib.mkIf (cfg.enable && isInstalled) {
     xdg.configFile."aerospace/aerospace.toml" = {
-      source = settingsFile;
+      source = lib.trace (builtins.readFile settingsFile) settingsFile;
     };
 
     home.activation.reloadAerospace =
