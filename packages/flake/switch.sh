6ec09ea04e88510ddccdbccd8de1bdef132e7f21
Michael Utz <michael@theutz.com>
2024-11-08 10:27:05 +0300

    Generation 23

--- packages/flake/switch.sh
+++ packages/flake/switch.sh
@@ -1,3 +1,5 @@
+@loggers@
+
 help() {
 	gum format <<-'markdown'
 		# @main@ @name@
@@ -17,3 +19,17 @@ help() {
 		| --help | -h    | show this help |
 	markdown
 }
+
+args=()
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+	--help | -h)
+		help
+		exit 0
+		;;
+	*)
+		fatal "argument not found: $1"
+		;;
+	esac
+done
+set -- "''${args[@]}"
