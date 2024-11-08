aa5df6954c5f5b4a36274067ae095aa3f78ce14d
Michael Utz <michael@theutz.com>
2024-11-08 21:43:02 +0300

    Generation 82

--- modules/home/aerospace/default.nix
+++ modules/home/aerospace/default.nix
@@ -7,19 +7,38 @@
 }: let
   mod = "aerospace";
   cfg = config.internal.${mod};
-
   isInstalled = lib.pipe osConfig.homebrew.casks [
     (lib.map (lib.getAttr "name"))
     (lib.elem mod)
   ];
 
-  settings = {
-    start-at-login = true;
+  borders = lib.getExe config.services.jankyborders.package or pkgs.jankyborders;
+  sketchybar = lib.getExe pkgs.sketchybar;
+
+  colors = {
+    active = "0xff7dcff";
+    inactive = "0x88a9b1d6";
+  };
+
+  mkArgs = lib.cli.toGNUCommandLineShell {};
+  defaultBorders = {
+    active_color = colors.active;
+    inactive_color = colors.inactive;
+    blur_radius = 12;
+    width = 8.0;
+    hidipi = true;
+    style = "round";
   };
 
   settingsFormat = pkgs.formats.toml {};
-
   settingsFile = settingsFormat.generate "aerospace.toml" settings;
+  settings = {
+    start-at-login = true;
+    after-startup-command = lib.forEach [
+      (lib.concatStrings [borders (mkArgs defaultBorders)])
+      (lib.concatStrings [sketchybar (mkArgs {reload = true;})])
+    ] (x: "exec-and-forget ${x}");
+  };
 in {
   options.internal.${mod} = {
     enable = lib.mkEnableOption "tiling window manager for darwin";
