bf498743762ae80f11387b87f5b539a63be33a2f
Michael Utz <michael@theutz.com>
2024-11-08 10:36:39 +0300

    Generation 27

--- packages/flake/switch.sh
+++ packages/flake/switch.sh
@@ -20,11 +20,29 @@ help() {
 	markdown
 }
 
+cleanup() {
+	if [[ -d "$MY_FLAKE_DIR/result" ]]; then
+		info "cleaning up old builds..."
+		rm -rf "$MY_FLAKE_DIR/result"
+	fi
+
+	if [[ "$(git log -1 --pretty=%B)" == "WIP" ]]; then
+		info "uncommitting WIP changes..."
+		git reset HEAD~
+	fi
+}
+
+trap cleanup EXIT
+
 args=()
 while [[ $# -gt 0 ]]; do
 	case "$1" in
 	--help | -h)
 		show_help=true
+		shift 1
+		;;
+	--force | -f)
+		force=true
 		shift 1
 		;;
 	--* | -*)
@@ -42,4 +60,16 @@ set -- "${args[@]}"
 if [[ "${show_help-false}" ]]; then
 	help
 	exit 0
+fi
+
+if ! cd "$MY_FLAKE_DIR"; then
+	error "failed to cd " "dir" "$MY_FLAKE_DIR"
+	fatal "exiting"
+fi
+
+if [[ ! "${force-false}" ]]; then
+	if [[ -z "$(git -c color.status=always status --short | tee /dev/tty)" ]]; then
+		warn "no changes detected. Exiting..."
+		exit 0
+	fi
 fi
