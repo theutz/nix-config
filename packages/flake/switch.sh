da9f012db725b595c392eac2d238f35317ed38ba
Michael Utz <michael@theutz.com>
2024-11-08 13:57:24 +0300

    Generation 61

--- packages/flake/switch.sh
+++ packages/flake/switch.sh
@@ -24,19 +24,15 @@ help() {
 
 cleanup() {
 	debug "cleaning up..."
-	echo
 
 	if [[ -d "$MY_FLAKE_DIR/result" ]]; then
 		info "cleaning up old builds..."
 		rm -rf "$MY_FLAKE_DIR/result"
-		echo
 	fi
 
 	if [[ "$(git log -1 --pretty=%B)" == "WIP" ]]; then
-		echo
 		info "uncommitting WIP changes..."
 		git reset HEAD~
-		echo
 	fi
 
 	debug "cleaned up"
@@ -92,53 +88,37 @@ fi
 
 if [[ "${force-false}" == false ]]; then
 	info "checking git status..."
-	echo
 	if [[ -z "$(git -c color.status=always status --short | tee /dev/tty)" ]]; then
 		warn "no changes detected. Exiting..."
 		exit 0
 	fi
-	echo
 fi
 
 info "creating WIP commit..."
-echo
 git add -A && git commit -m "WIP"
-echo
 
 info "switching to new generation..."
-echo
 if darwin-rebuild switch --flake .; then
-	echo
 	info "profile switched"
 else
-	echo
 	error "failure while changing profile"
 	fatal "exiting"
 fi
-echo
 
 current_generation="$(darwin-rebuild --list-generations | awk '/\(current\)/ {print $1}')"
 
 info "committing changes..."
-echo
 if git commit --amend --message "Generation $current_generation"; then
-	echo
 	info "changes committed"
 else
-	echo
 	error "changes could not be committed"
 	fatal exiting
 fi
-echo
 
 info "pushing changes"
-echo
 if git pull --rebase && git push; then
-	echo
 	info "changes pushed"
 else
-	echo
 	error "changes could not be pushed"
 	fatal exiting
 fi
-echo
