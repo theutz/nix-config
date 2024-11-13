9f8b44dc3da6c1a840ec9b8eff19d868eab7275b
Michael Utz <michael@theutz.com>
2024-11-13 14:40:42 +0300

    Generation 223

--- modules/home/jrnl/default.nix
+++ modules/home/jrnl/default.nix
@@ -7,6 +7,42 @@
 }: let
   mod = lib.${namespace}.path.getLastComponent ./.;
   cfg = config.${namespace}.${mod};
+
+  yamlFormat = pkgs.formats.yaml {};
+
+  mkJrnlPath = x: (lib.concatStringsSep "/"
+    [
+      config.home.homeDirectory
+      "Dropbox"
+      "jrnl"
+      (lib.concatStrings [x ".txt"])
+    ]);
+
+  settings = {
+    colors = {
+      body = "nones";
+      date = "black";
+      tags = "yellow";
+      title = "cyan";
+    };
+    default_hour = 9;
+    default_minute = 0;
+    editor = config.home.sessionVariables.EDITOR;
+    encrypt = true;
+    highlight = true;
+    indent_character = "|";
+    journals = {
+      default = {
+        journal = mkJrnlPath "settings.txt";
+        delegator = mkJrnlPath "delegator.txt";
+      };
+    };
+    linewrap = 79;
+    tagsymbols = "#@";
+    template = false;
+    timeformat = "%F %r";
+    version = "v4.1";
+  };
 in {
   options.${namespace}.${mod}.enable = lib.mkEnableOption mod;
 
@@ -14,6 +50,6 @@ in {
     home.packages = with pkgs; [jrnl];
 
     xdg.configFile."jrnl/jrnl.yaml".source =
-      config.lib.${namespace}.mkOutOfStoreSymlink ./jrnl.yaml;
+      yamlFormat.generate "jrnl.yaml" settings;
   };
 }
